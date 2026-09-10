d924cd82564150999be518713fd665183bfdab76
dn-lx <don.alex.perera@gmail.com>
2026-09-10 11:41:22 +0200

    Add FrankiFlow and FrankiHolz brand sources

---
+++ src/brand.ts
@@ -0,0 +1,19 @@
+export const APP_NAME = 'FrankiFlow-Admin';
+export const APP_VERSION = '1.2.0';
+
+export const brand = {
+  frankiflow: {
+    name: 'FrankiFlow',
+    subtitleDe: 'Gebäudereinigung & Objektbetreuung',
+    subtitleEn: 'Cleaning & property services',
+    tagline: 'Mehr als Reinigung',
+    logoUri: 'https://drive.google.com/uc?export=view&id=1wH39Pd9N5G_ig7BfLYNcYGVLQyzNhLlU',
+  },
+  frankiholz: {
+    name: 'FrankiHolz',
+    subtitleDe: 'Unterkunft & Direktbuchung',
+    subtitleEn: 'Accommodation & direct booking',
+    tagline: 'Ankommen. Wohlfühlen.',
+    logoUri: 'https://drive.google.com/uc?export=view&id=1mjoKa_-AZb0ONfpIXQPe7DOBydCffeXB',
+  },
+};
